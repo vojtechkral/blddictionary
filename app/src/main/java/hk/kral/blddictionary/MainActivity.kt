86adab1e6aca0d9a5c399157443260a65d719bf7
tomkral <tomas@kral.hk>
2016-12-30 21:01:29 +0100

    Added handler for bSave.

--- app/src/main/java/hk/kral/blddictionary/MainActivity.kt
+++ app/src/main/java/hk/kral/blddictionary/MainActivity.kt
@@ -2,8 +2,13 @@ package hk.kral.blddictionary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var pair: String = "AB"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
         val dict = Dictionary()
 
+        val bSave: Button = findViewById(R.id.bSave) as Button
+        val etWord: EditText = findViewById(R.id.etWord) as EditText
 
+        bSave.setOnClickListener({view -> dict.setWord(pair, etWord.text.toString())})
     }
 }
